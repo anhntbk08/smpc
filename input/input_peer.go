ca8f37d92e60a2d26b6d160fccc7e49f7677e090
Aurojit Panda <apanda@cs.berkeley.edu>
2013-04-18 16:42:41 -0700

    Refactor and merge

--- input/input_peer.go
+++ input/input_peer.go
@@ -8,7 +8,7 @@ import (
         "os"
         "os/signal"
         "sync"
-        "time"
+        "strings"
         )
 
 type InputPeerState struct {
@@ -132,63 +132,41 @@ func EventLoop (config *string, state *InputPeerState, q chan int, ready chan bo
     }()
 }
 
-func circuit (state *InputPeerState, topo *Topology, end_channel chan int) {
-    val := int64(0)
-    _ = val
-    //topo := state.MakeTestTopology(end_channel)  
-    
-    nnhop := make(map[int64] string, len(topo.AdjacencyMatrix))
-    elapsed := float64(0)
-    iters :=12
-    for it := 0; it < iters; it++ {
-        t := time.Now()
-        nnhop = make(map[int64] string, len(topo.AdjacencyMatrix))
-        ch := make(map[int64] chan string, len(topo.AdjacencyMatrix))
-        for i := range topo.AdjacencyMatrix {
-            ch[i] = state.RunSingleIteration(topo, i, end_channel)
-        }
-        for i  := range topo.AdjacencyMatrix {
-            nnhop[i] = <- ch[i]
-        }
-        topo.NextHop = nnhop
-        elapsed += (time.Since(t).Seconds())
-    }
-
-    fmt.Printf("Two round NextHop, should be 2, 2, 2, 1 Time: %f\n", elapsed/float64(iters))
-    for ind := range nnhop {
-        c42 := state.GetValue(nnhop[ind], end_channel)
-        val = <- c42
-        fmt.Printf("%d: %d\n", ind, val)
-    }
-    
-    end_channel <- 0
-}
-
 func main() {
     // Start up by setting up a flag for the Configuration file
     config := flag.String("config", "conf", "Configuration file")
     topoFile := flag.String("topo", "", "Topology file")
     dest := flag.Int64("dest", 0, "Destination")
     flag.Parse()
-    jsonTopo := ParseJsonTopology(topoFile)  
     os_channel := make(chan os.Signal)
     signal.Notify(os_channel)
+    configs := strings.Split(*config, " ")
+    state := make([]*InputPeerState, len(configs))
+    coordinate_channel := make([]chan bool, len(configs))
+    fmt.Printf("Config files :%s\n", *config)
     end_channel := make(chan int, 1)
-    coordinate_channel := make(chan bool)
-    state := &InputPeerState{}
-    go EventLoop(config, state, end_channel, coordinate_channel)
     var status = 0
+    for i := range configs {
+        state[i] = &InputPeerState{}
+        coordinate_channel[i] = make(chan bool)
+        go EventLoop(&configs[i], state[i], end_channel, coordinate_channel[i])
+    }
+    for ch := range coordinate_channel {
+        select {
+            case <- coordinate_channel[ch]:
+                continue
+            case status = <- end_channel: 
+                os.Exit(status)
+            case <- os_channel:
+                os.Exit(status)
+        }
+    }
+    fmt.Printf("All reported in, going to start\n")
+    go circuit(state, topoFile, *dest, end_channel)
     for {
         select {
-            case <- coordinate_channel:
-                // Now ready to execute
-                topo := jsonTopo.MakeTopology(state, end_channel)
-                if *dest != 0 {
-                    ch := state.SetValue(topo.NextHop[*dest], *dest, end_channel)
-                    <- ch
-                }
-                go circuit(state, topo, end_channel)
             case status = <- end_channel: 
+                fmt.Printf("Exiting for some reason internal to us")
                 os.Exit(status)
             case <- os_channel:
                 os.Exit(status)
---
+++ input/smirc_circuit.go
@@ -0,0 +1,45 @@
+package main
+import (
+        "fmt"
+        "time"
+        )
+
+func circuit (states []*InputPeerState, topoFile *string, dest int64, end_channel chan int) {
+    state := states[0]
+    val := int64(0)
+    _ = val
+    jsonTopo := ParseJsonTopology(topoFile)  
+    topo := jsonTopo.MakeTopology(state, end_channel)
+    if dest != 0 {
+        ch := state.SetValue(topo.NextHop[dest], dest, end_channel)
+        <- ch
+    }
+    //topo := state.MakeTestTopology(end_channel)  
+    
+    nnhop := make(map[int64] string, len(topo.AdjacencyMatrix))
+    elapsed := float64(0)
+    iters :=12
+    for it := 0; it < iters; it++ {
+        t := time.Now()
+        nnhop = make(map[int64] string, len(topo.AdjacencyMatrix))
+        ch := make(map[int64] chan string, len(topo.AdjacencyMatrix))
+        for i := range topo.AdjacencyMatrix {
+            ch[i] = state.RunSingleIteration(topo, i, end_channel)
+        }
+        for i  := range topo.AdjacencyMatrix {
+            nnhop[i] = <- ch[i]
+        }
+        topo.NextHop = nnhop
+        elapsed += (time.Since(t).Seconds())
+    }
+
+    fmt.Printf("Two round NextHop, should be 2, 2, 2, 1 Time: %f\n", elapsed/float64(iters))
+    for ind := range nnhop {
+        c42 := state.GetValue(nnhop[ind], end_channel)
+        val = <- c42
+        fmt.Printf("%d: %d\n", ind, val)
+    }
+    
+    end_channel <- 0
+}
+

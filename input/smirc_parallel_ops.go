ba2827ee10abe0c17d4633bab9c0615e5185295a
Aurojit Panda <apanda@cs.berkeley.edu>
2013-04-19 09:39:04 -0700

    Parallel topology

--- input/smirc_parallel_ops.go
+++ input/smirc_parallel_ops.go
@@ -21,9 +21,16 @@ func BroadcastSetValue (states []*InputPeerState, result string, value int64, q 
 func (json *JsonTopology) MakeBroadcastTopology (states []*InputPeerState, q chan int) ([]*Topology) {
     topos := make([]*Topology, len(states))
     fmt.Printf("Constructing topology\n")
+    chans := make([]chan bool, len(topos))
     for i := range topos {
-        topos[i] = json.MakeTopology(states[i], q)
-        fmt.Printf("Done constructing topology %d\n", i)
+        chans[i] = make(chan bool, 1)
+        go func(i int) {
+            topos[i] = json.MakeTopology(states[i], q)
+            chans[i] <- true
+        }(i)
+    }
+    for i := range chans {
+        <- chans[i]
     }
     return topos
 }
